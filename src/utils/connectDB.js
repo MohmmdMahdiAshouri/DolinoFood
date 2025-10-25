7a04300b61bb8232473fee4e43d50e1c7220625c
MohmmdMahdiAshouri <mohammadmahdiashouri80@gmail.com>
2025-10-25 10:39:59 +0330

    forth commit

--- src/utils/connectDB.js
+++ src/utils/connectDB.js
@@ -1,9 +1,14 @@
-import { mongoose }  from "mongoose";
+import { mongoose } from "mongoose";
 
 async function connectDB() {
-    if(mongoose.connections[0].readyState === 1) return
-    mongoose.set("strictQuery" , false)
-    await mongoose.connect("mongodb://localhost:27017/dolino")
+    if (mongoose.connections[0].readyState === 1) return;
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.MONGOURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+
+    console.log("✅ Connected to MongoDB Atlas");
 }
 
-export default connectDB
+export default connectDB;
